5d2e3ab6ec0560bf38cba56afbbaafb43b6491bc
Andrew O'Connor <andrew.oconnor@gmail.com>
2016-03-01 17:36:17 +0000

    Moved api handlers into api package

--- main.go
+++ main.go
@@ -1,69 +1,20 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-
 	"bitbucket.org/andoco/gomailservice/api"
 	"bitbucket.org/andoco/gomailservice/delivery"
-	"bitbucket.org/andoco/gomailservice/job"
 	"bitbucket.org/andoco/gomailservice/queue"
 
-	"github.com/goji/param"
-	"github.com/satori/go.uuid"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/graceful"
-	"github.com/zenazn/goji/web"
 )
-
-func hello(c web.C, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello asdf, %s!", c.URLParams["name"])
-}
-
-func postMail(c web.C, w http.ResponseWriter, r *http.Request) {
-	var msg delivery.MailMessage
-
-	r.ParseForm()
-	err := param.Parse(r.Form, &msg)
-
-	if err != nil || len(msg.Message) > 140 {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	msg.Id = uuid.NewV1().String()
-
-	queue.Enqueue(&msg)
-
-	resource := api.MailMessageResource{Msg: &msg}
-
-	encoder := json.NewEncoder(w)
-	encoder.Encode(resource)
-}
-
-func postJob(c web.C, w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-
-	var mailjob job.Job
-	if err := decoder.Decode(&mailjob); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	if err := job.Process(mailjob); err != nil {
-		log.Printf("error handling postJob; %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
 
 func main() {
 	queue.Start()
 
-	goji.Get("/hello/:name", hello)
-	goji.Post("/mail", postMail)
-	goji.Post("/job", postJob)
+	goji.Get("/hello/:name", api.Hello)
+	goji.Post("/mail", api.PostMail)
+	goji.Post("/job", api.PostJob)
 
 	graceful.PostHook(func() {
 		queue.Stop()
